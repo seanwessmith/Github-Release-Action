7eee59eb43b240103a3d22c9d095063a515684a9
Sean Smith <seanwessmith@gmail.com>
2024-08-16 14:12:22 -0700

    fix: cleanup release.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Fail on any error.
+set -e
+
+echo "Setting up workdir ${INPUT_WORKDIR}"
 if [ ! -z "${INPUT_WORKDIR}" ]; then
+    echo "Changing working directory to ${INPUT_WORKDIR}"
     cd "${INPUT_WORKDIR}"
 fi
 
